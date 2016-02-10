8d5dded45396fbc65f86909569c999e54a0ef8a6
joharijumali <johari@3fresources.com>
2016-02-10 14:40:08 +0800

    update composer.json

--- migrations/2015_10_02_083158_create_user_trail_in_table.php
+++ migrations/2015_10_02_083158_create_user_trail_in_table.php
@@ -19,6 +19,7 @@ class CreateUserTrailInTable extends Migration
             $table->integer('type')->nullable();
             $table->integer('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
         });
     }
 

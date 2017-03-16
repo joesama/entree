eabcf5f187f496b4f4418fe2fd6a807b499fe799
Johari <joharijumali@gmail.com>
2017-03-17 04:37:47 +0800

    update default value nullable

--- resources/database/migrations/2015_10_02_083158_create_user_trail_in_table.php
+++ resources/database/migrations/2015_10_02_083158_create_user_trail_in_table.php
@@ -24,7 +24,7 @@ class CreateUserTrailInTable extends Migration
             
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->string('person');
+            $table->string('person')->nullable();
             $table->integer('type')->nullable();
             $table->integer('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users');

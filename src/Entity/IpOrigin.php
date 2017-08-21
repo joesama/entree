245f7d684d426178ed789e49354cd5a9d2b222ee
Johari <joharijumali@gmail.com>
2017-08-21 10:41:28 +0800

    function called

--- src/Entity/IpOrigin.php
+++ src/Entity/IpOrigin.php
@@ -22,7 +22,7 @@ class IpOrigin
     public function ipOrigin()
     {
         // check for shared internet/ISP IP
-        if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
+        if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
             return $_SERVER['HTTP_CLIENT_IP'];
         }
         // check for IPs passing through proxies
@@ -31,21 +31,21 @@ class IpOrigin
             if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') !== false) {
                 $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
                 foreach ($iplist as $ip) {
-                    if (validate_ip($ip))
+                    if ($this->validate_ip($ip))
                         return $ip;
                 }
             } else {
-                if (validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
+                if ($this->validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
                     return $_SERVER['HTTP_X_FORWARDED_FOR'];
             }
         }
-        if (!empty($_SERVER['HTTP_X_FORWARDED']) && validate_ip($_SERVER['HTTP_X_FORWARDED']))
+        if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))
             return $_SERVER['HTTP_X_FORWARDED'];
-        if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+        if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
             return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-        if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+        if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
             return $_SERVER['HTTP_FORWARDED_FOR'];
-        if (!empty($_SERVER['HTTP_FORWARDED']) && validate_ip($_SERVER['HTTP_FORWARDED']))
+        if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))
             return $_SERVER['HTTP_FORWARDED'];
         // return unreliable ip since all else failed
         return $_SERVER['REMOTE_ADDR'];

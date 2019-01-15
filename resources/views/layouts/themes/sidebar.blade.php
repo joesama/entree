c33ba388b69e84eed5fd097b9533e7ca965c7c42
Johari <joharijumali@gmail.com>
2019-01-15 08:47:42 +0800

    adjust shortcut redirection

--- resources/views/layouts/themes/sidebar.blade.php
+++ resources/views/layouts/themes/sidebar.blade.php
@@ -47,29 +47,29 @@
             <!--================================-->
             <div id="mainnav-shortcut">
                 <ul class="list-unstyled shortcut-wrap">
-                    <li class="col-xs-3" data-content="{{trans('joesama/entree::title.home',['home'=> ucwords(config('joesama/entree::entree.landing','home'))])}}">
+                    <li class="col-xs-3" data-content="{{trans('joesama/entree::title.home')}}">
                         <a class="shortcut-grid" href="{!! handles('joesama/entree::'.config('joesama/entree::entree.landing','home')) !!}">
                             <div class="icon-wrap icon-wrap-sm icon-circle bg-mint">
-                            <i class="pli-dashboard"></i>
+                            <i class="psi-home"></i>
                             </div>
                         </a>
                     </li>
-                    <li class="col-xs-3" data-content="{{trans('joesama/project::project.dashboard.overall')}}">
-                        <a class="shortcut-grid" href="{!! handles('joesama/project::dashboard') !!}"">
+                    <li class="col-xs-3" data-content="{{trans('joesama/project::manager.dashboard.overall')}}">
+                        <a class="shortcut-grid" href="{!! handles('joesama/project::manager/dashboard/overall/'.request()->segment(4)) !!}"">
                             <div class="icon-wrap icon-wrap-sm icon-circle bg-warning">
                             <i class="pli-project"></i>
                             </div>
                         </a>
                     </li>
                     <li class="col-xs-3" data-content="{{ trans('joesama/project::project.task.task') }}">
-                        <a class="shortcut-grid" href="#">
+                        <a class="shortcut-grid" href="{!! handles('joesama/project::manager/task/list/'.request()->segment(4)) !!}">
                             <div class="icon-wrap icon-wrap-sm icon-circle bg-success">
                             <i class="pli-calendar"></i>
                             </div>
                         </a>
                     </li>
                     <li class="col-xs-3" data-content="{{ trans('joesama/project::project.issues.name') }}">
-                        <a class="shortcut-grid" href="#">
+                        <a class="shortcut-grid" href="{!! handles('joesama/project::manager/issue/list/'.request()->segment(4)) !!}">
                             <div class="icon-wrap icon-wrap-sm icon-circle bg-purple">
                             <i class="pli-calendar-clock"></i>
                             </div>

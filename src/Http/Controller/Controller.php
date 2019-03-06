a305fe77778d3e905e67cfa94a36b94f2e97ed88
Johari <joharijumali@gmail.com>
2019-03-07 05:28:09 +0800

    merge orchestra 3.8

--- src/Http/Controller/Controller.php
+++ src/Http/Controller/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Orchestra\Routing\Traits\ControllerResponse;
+use Orchestra\Routing\Concerns\ControllerResponse;
 
 abstract class Controller extends BaseController
 {
